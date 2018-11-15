517ff05195bf818aeba993bd3ed5d529858b7cdd
Aaron Zheng <aaronzheng3@gmail.com>
2018-11-14 21:18:27 -0500

    footer progress

--- frontend/components/splash/footer.jsx
+++ frontend/components/splash/footer.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 
 class Footer extends React.Component {
     render () {
@@ -8,7 +9,19 @@ class Footer extends React.Component {
               <a href="/" className="lolgo">Whimsy Notes</a>
             </div>
             <hr className="first-break"/>
-          </div>
+              <div className="session-div2">
+                <span className="session-links session-move"><Link className="splash-signup" to="/signup">Sign up</Link><span className="or">or</span><Link className="splash-login" to="/login">Log in</Link></span>
+              </div>
+            <hr className="second-break" />
+              <div className="copyright-div">
+                <p className="copyright">2018 Whimsy Notes Corporation. All tights reserved.</p>
+                <ul className="copyright-list">
+                  <li>Security</li>
+                  <li>Legal</li>
+                  <li>Privacy</li>
+                </ul>
+              </div>
+            </div>
       )
     };
 }
