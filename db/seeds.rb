09e19ea1a712111ea210438216d93036c6de5f07
Aaron Zheng <aaronzheng3@gmail.com>
2018-11-20 11:30:48 -0500

    updated seed data with previews

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,14 +17,17 @@ TagNote.destroy_all
 demo = User.create(username: "demoman", password: "invisible")
 shower = Notebook.create(title: "Showerthoughts", user_id: demo.id)
 app = Notebook.create(title: "AppAcademy Notes", user_id: demo.id)
-sheep = Note.create(title: "Duality", body: "Do the sheep count sheep when they sleep?", user_id: demo.id, notebook_id: shower.id)
+sheep = Note.create(title: "Duality", body: "Do the sheep count sheep when they sleep?", user_id: demo.id, notebook_id: shower.id, preview:"Do the sheep count sheep when they sleep?")
 laotze = Note.create(title: "Reflection on Reality", body: "Once upon a time, I dreamt I was a butterfly, fluttering hither and thither, to all intents and
         purposes a butterfly. I was conscious only of my happiness as a butterfly, unaware that I was myself.
         Soon I awaked, and there I was, veritably myself again. Now I do not know whether I was then a man
-        dreaming I was a butterfly, or whether I am now a butterfly, dreaming I am a man.", user_id: demo.id, notebook_id: shower.id)
-check = Note.create( title: "To do", body: "Double check my proposal!", user_id: demo.id, notebook_id: app.id)
-mind = Note.create(title: "Reflection on Mind", body: "Is the mind and brain connected?", user_id: demo.id, notebook_id: shower.id)
-soul = Note.create(title: "Reflection on Soul", body:"The soul is a component of our being much like our mind but has no physical manifestation which it is linked to.", user_id: demo.id, notebook_id: shower.id)
+        dreaming I was a butterfly, or whether I am now a butterfly, dreaming I am a man.", user_id: demo.id, notebook_id: shower.id, preview:"Once upon a time, I dreamt I was a butterfly, fluttering hither and thither, to all intents and
+                purposes a butterfly. I was conscious only of my happiness as a butterfly, unaware that I was myself.
+                Soon I awaked, and there I was, veritably myself again. Now I do not know whether I was then a man
+                dreaming I was a butterfly, or whether I am now a butterfly, dreaming I am a man." )
+check = Note.create( title: "To do", body: "Double check my proposal!", user_id: demo.id, notebook_id: app.id, preview:"Double check my proposal!")
+mind = Note.create(title: "Reflection on Mind", body: "Is the mind and brain connected?", user_id: demo.id, notebook_id: shower.id, preview:"Is the mind and brain connected?")
+soul = Note.create(title: "Reflection on Soul", body:"The soul is a component of our being much like our mind but has no physical manifestation which it is linked to.", user_id: demo.id, notebook_id: shower.id preview:"The soul is a component of our being much like our mind but has no physical manifestation which it is linked to.")
 philo = Tag.create!(tag: "philosophy", user_id:demo.id)
 whim = Tag.create!(tag: "whimsical", user_id:demo.id)
 code = Tag.create!(tag: "coding", user_id:demo.id)
